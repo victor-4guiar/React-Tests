692d382c309061e9e565a8edb8cbbf54bd4bd1a0
Victor Aguiar <victoraguiarrosa397@gmail.com>
2022-09-24 16:35:19 -0300

    foof

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import styles from './index.css';
 import Header from './components/header/Header';
 import Engre from './components/items/Engre';
+import For from './components/For';
 
 function App(){
 	return(
 		<>
 			<Header companyName="Logo" navList={["Home", "Company", "Contact"]}/>
-			<Engre quant='4'/>
+			<Engre quant='4' />
+			<For />
 		</>
 	);
 }
